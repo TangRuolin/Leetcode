102d1bddd4862655bd2ccab9027f155cd43e3efa
tangruolin <190935423@qq.com>
2019-12-18 20:33:04 +0800

    2019.12.18

--- Text/Text/Program.cs
+++ Text/Text/Program.cs
@@ -8,10 +8,19 @@ namespace Text
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[4] { 0,1,2,-3 };
-            int re = ThreeSumClosest(nums,1);
+            //string a = "23";
+            IList<string> a = GenerateParenthesis(3);
             Console.ReadKey();
         }
+        #region 题目中用到的节点（ListNode）
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+        #endregion
+
         #region 求两数和
         public int[] TwoSum(int[] nums, int target)
         {
@@ -181,12 +190,7 @@ namespace Text
         #endregion
 
         #region 两数相加
-        public class ListNode
-        {
-             public int val;
-             public ListNode next;
-             public ListNode(int x) { val = x; }
-         }
+       
         static public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
             ListNode result = new ListNode(0);
@@ -353,7 +357,7 @@ namespace Text
         }
         #endregion
 
-        #region 最接近的三数之和
+        #region 最接近的三数之和（用排序加双指针）
         public static int ThreeSumClosest(int[] nums, int target)
         {
             List<int> numList = nums.ToList();
@@ -369,21 +373,301 @@ namespace Text
                 while (max - min > 0)
                 {
                     int newNum = numList[i] + numList[min] + numList[max];
-                    int newNumDis = Math.Abs(target - newNum);
-                    if(numDis<0||newNumDis < numDis)
+                    int newNumDis = target - newNum;
+                    if(numDis<0||Math.Abs(newNumDis) < numDis)
                     {
-                        numDis = newNumDis;
+                        numDis = Math.Abs(newNumDis);
                         num = newNum;
                     }
                     if (newNumDis == 0) return num;
-                    else
-                    {
-                        if (newNum > 0) max--;
-                        else if (newNum < 0) min--;
-                    }
+                    else if (newNumDis > 0) min++;
+                    else if (newNumDis < 0) max--;
                 }
             }
             return num;
+        }
+        #endregion
+
+        #region 电话号码的字母组合(用回溯算法解决)
+        static List<string> LetterCombinationsResult = new List<string>();
+        public static IList<string> LetterCombinations(string digits)
+        {
+            string[] reTable = new string[10] {
+                "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
+            };
+            if (string.IsNullOrEmpty(digits)) return LetterCombinationsResult;
+            char[] d = digits.ToCharArray();
+            LetterCom(d,reTable,0,"");
+            return LetterCombinationsResult;
+        }
+       static void LetterCom(char[] digits,string[] reTable,int index,string resultString)
+        {
+            if(index >= digits.Length)
+            {
+                LetterCombinationsResult.Add(resultString);
+                return;
+            }
+            int indexRe = digits[index]-48;
+            char[] nowTable = reTable[indexRe].ToCharArray();
+            for(int i = 0; i < nowTable.Length; i++)
+            {
+                resultString += nowTable[i];
+                LetterCom(digits, reTable, index + 1, resultString);
+                resultString = resultString.Substring(0,resultString.Length-1);
+            }
+        }
+        #endregion
+
+        #region 删除链表的倒数第N个节点
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (n == 0) return head;
+            if (head == null) return null;
+            if (head.next == null) return null;
+
+            int count = 0;
+            ListNode node = head;
+            ListNode node2 = null;
+            while (node != null)
+            {
+                node = node.next;
+                count++;
+                if (count > n)
+                {
+                    node2 = node2 == null ? head : node2.next;
+                }
+                if (node == null)
+                {
+                    if (node2 == null) return head.next;
+                    node2.next = node2.next.next;
+                }
+            }
+            return head;
+        }
+        #endregion
+
+        #region 有效括号
+        public static bool IsValid(string s)
+        {
+            char[] sChar = s.ToCharArray();
+            List<char> buffer = new List<char>();
+            for(int i = 0; i < sChar.Length; i++)
+            {
+                if(sChar[i] == ']')
+                {
+                    if (buffer.Count>0 && buffer[buffer.Count - 1] == '[')
+                    {
+                        buffer.RemoveAt(buffer.Count-1);
+                    }
+                    else return false;
+                }
+                else if( sChar[i] == '}')
+                {
+                    if (buffer.Count > 0 && buffer[buffer.Count - 1] == '{')
+                    {
+                        buffer.RemoveAt(buffer.Count - 1);
+                    }
+                    else return false;
+                }
+                else if ( sChar[i] == ')')
+                {
+                    if (buffer.Count > 0 && buffer[buffer.Count - 1] == '(')
+                    {
+                        buffer.RemoveAt(buffer.Count - 1);
+                    }
+                    else return false;
+                }
+                else if(sChar[i] == '(' || sChar[i] == '{' || sChar[i] == '[')
+                {
+                    buffer.Add(sChar[i]);
+                }
+            }
+            if (buffer.Count > 0) return false;
+            return true;
+        }
+        #endregion
+
+        #region 合并两个有序链表
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode(0);
+            ListNode cur = result;
+            while(l1 != null && l2 != null)
+            {
+                if(l1.val <= l2.val)
+                {
+                    cur.next = l1;
+                    cur = cur.next;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    cur.next = l2;
+                    cur = cur.next;
+                    l2 = l2.next;
+                }
+            }
+            if (l1 != null) cur.next = l1;
+            if (l2 != null) cur.next = l2;
+            return result.next;
+        }
+        #endregion
+
+        #region 生成括号的对数
+        static List<string> result = new List<string>();
+        public static IList<string> GenerateParenthesis(int n)
+        {
+            if (n == 0) return result;
+            string reString = "";
+            List<char> buffer = new List<char>();
+            int count = 0;
+            GeneratePa(reString,buffer,n,count);
+            return result;
+        }
+        static void GeneratePa(string reString,List<char> buffer,int n,int count)
+        {
+            if(count == n)
+            {
+                for(int i = 0; i < buffer.Count; i++)
+                {
+                    reString += ')';
+                }
+                result.Add(reString);
+                return;
+            }
+           
+            for(int i = 0; i < 2; i++)
+            {
+                if(i == 0)
+                {
+                    buffer.Add('(');
+                    reString += '(';
+                    count++;
+                    GeneratePa(reString, buffer,n,count);
+                    if (buffer.Count == 0) continue;
+                    buffer.RemoveAt(buffer.Count-1);
+                    reString = reString.Substring(0,reString.Length-1);
+                    count--;
+                }
+                else
+                {
+                    if (buffer.Count == 0)
+                    {
+                        continue;
+                    }
+                    buffer.RemoveAt(buffer.Count - 1);
+                    reString += ')';
+                    GeneratePa(reString, buffer, n, count);
+                    buffer.Add('(');
+                }
+                
+            }
+        }
+        #endregion
+
+        #region 合并k个排序链表(用到合并两个有序链表的方法)
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            if (lists.Length == 0) return null;
+            if (lists.Length == 1) return lists[0];
+            ListNode[] newLists;
+            if (lists.Length % 2 == 0)
+            {
+                newLists = new ListNode[lists.Length / 2];
+            }
+            else
+            {
+                newLists = new ListNode[lists.Length / 2 + 1];
+                newLists[lists.Length / 2] = lists[lists.Length / 2];
+            }
+            for(int i = 0; i < lists.Length / 2; i++)
+            {
+                newLists[i] = MergeTwoLists(lists[i],lists[lists.Length-i-1]);
+            }
+            return MergeKLists(newLists);
+        }
+        #endregion
+
+        #region 两两交换链表中的节点
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+            ListNode result = new ListNode(0);
+            ListNode cur = result;
+            while(head != null && head != null)
+            {
+
+            }
+            return result.next;
+        }
+        #endregion
+
+        #region 删除排序数组中的重复项(双指针)
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return 1;
+            int start = 0;
+            int end = 1;
+            while(end < nums.Length)
+            {
+                if(nums[end] == nums[start])
+                {
+                    end++;
+                }
+                else
+                {
+                    start++;
+                    nums[start] = nums[end];
+                    end++;
+                }
+            }
+            return start+1;
+        }
+        #endregion
+
+        #region 移除元素
+        public int RemoveElement(int[] nums, int val)
+        {
+            int newId = 0;
+            int nowId = 0;
+            while(nowId < nums.Length)
+            {
+                if (nums[nowId] == val) nowId++;
+                else
+                {
+                    nums[newId] = nums[nowId];
+                    newId++;
+                    nowId++;
+                }
+            }
+            return newId;
+        }
+        #endregion
+
+        #region 搜索旋转排序数组
+        public int Search(int[] nums, int target)
+        {
+            if (nums == null || nums.Length == 0) return -1;
+            int min = 0, max = nums.Length;
+            int mid;
+            while(min <= max)
+            {
+                mid = (min + max) / 2;
+                if (nums[mid] == target) return mid;
+                if (nums[min] <= nums[mid])//前半部分有序
+                {
+                   
+                }
+                else  //后半部分有序
+                {
+
+                }
+            }
+            return -1;
         }
         #endregion
     }
